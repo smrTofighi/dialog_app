5e99038ca1cfe7a12fe78fc5beafbefd0e758ae0
smrTofighi <mrtofxn@gmail.com>
2022-08-11 22:41:40 +0430

    change chats view

--- lib/views/chats_view.dart
+++ lib/views/chats_view.dart
@@ -8,40 +8,74 @@ class ChatsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return SizedBox(
       height: Get.height,
       width: Get.width,
-      child: Stack(
+      child: Column(
         children: [
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Container(
-              width: Get.width,
-              height: Get.height / 1.6,
-              decoration: const BoxDecoration(
-                color: SolidColors.scaffoldBGColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(35),
-                  topRight: Radius.circular(35),
-                ),
-              ),
-              child: SizedBox(
-                width: Get.width,
-                height: Get.height,
-                child: Column(
-                  children: [
-                    const Align(
-                      child: Text('پیام های اخیر'),
-                      alignment: Alignment.centerRight,
-                    ),
-                    
-                  ],
-                ),
-              ),
+          SizedBox(
+            height: size.height / 5,
+            width: Get.width,
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'وضعیت',
+                        style: TextStyle(color: Colors.white, fontSize: 22),
+                      ),
+                      Text(
+                        'دیدن همه',
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
+          Container(
+            width: Get.width,
+            height: size.height / 1.7,
+            decoration: const BoxDecoration(
+              color: SolidColors.scaffoldBGColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'پیام های اخیر',
+                        style: TextStyle(fontSize: 22),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(100),
+                            ),
+                            color: Colors.yellow),
+                        child: const Text('بایگانی ها'),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
